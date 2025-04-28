86ed6fbae70f400f089b44881a0419bf09db9d96
irested <inxsize@live.fr>
2025-04-28 16:48:58 +0200

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,20 @@
+module.exports = function (eleventyConfig) {
+  // Permet de copier les fichiers statiques comme le CSS et les images
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/img");
+  eleventyConfig.addPassthroughCopy("src/admin");
+
+  // Redirige la page index.md pour qu'elle soit générée à la racine
+  eleventyConfig.addPassthroughCopy("src/pages/index.md");
+
+  // Configuration du dossier d'entrée et de sortie
+  return {
+    dir: {
+      input: "src", // Dossier source
+      output: "_site", // Dossier de sortie
+      includes: "_includes",
+      data: "_data",
+    },
+    passthroughFileCopy: true, // Assure-toi de copier tous les fichiers
+  };
+};
